2bdf4a478f3d613e5a9ecbb86267d95a62cdf1f2
Greg Parker <gregjp91@gmail.com>
2020-12-12 13:48:35 -0500

    Cleaned PyBank

--- PyBank/main.py
+++ PyBank/main.py
@@ -14,15 +14,8 @@ with open(bdpath) as bdfile:
     for row in bdreader:
         months += 1
         profit += int(row[1])
-
-with open(bdpath) as bdfile:
-    
-    bdreader = csv.reader(bdfile,delimiter=',')
-    next(bdreader)
-
-    for x in bdreader:
-        dates.append(x[0])
-        each_profit.append(int(x[1]))
+        dates.append(row[0])
+        each_profit.append(int(row[1]))
 
 with open(bdpath) as bdfile:
     
@@ -32,17 +25,17 @@ with open(bdpath) as bdfile:
     for x in range(1,months):
         change.append(each_profit[x]-each_profit[x-1])
 
-    average_change = round(sum(change)/len(change),2)
-    max_change = max(change)
-    min_change = min(change)
-    max_date = dates[change.index(max_change)+1]
-    min_date = dates[change.index(min_change)+1]
+average_change = round(sum(change)/len(change),2)
+max_change = max(change)
+min_change = min(change)
+max_date = dates[change.index(max_change)+1]
+min_date = dates[change.index(min_change)+1]
 
-   
-    line12 = f"Financial Analysis\n------------------"
-    line345 = f"Total Months: {months}\nTotal Profit: ${profit}\nAverage Change: ${average_change}"
-    line67 = f"Greatest Profit Increase: {max_date} (${max_change})\nGreatest Profit Decrease: {min_date} (${min_change})"
-    print(f"{line12}\n{line345}\n{line67}")    
- 
-    with open('analysis/budget_data.txt','w') as out:
-        out.writelines(f"{line12}\n{line345}\n{line67}")
+
+line12 = f"Financial Analysis\n------------------"
+line345 = f"Total Months: {months}\nTotal Profit: ${profit}\nAverage Change: ${average_change}"
+line67 = f"Greatest Profit Increase: {max_date} (${max_change})\nGreatest Profit Decrease: {min_date} (${min_change})"
+print(f"{line12}\n{line345}\n{line67}")    
+
+with open('analysis/budget_data.txt','w') as out:
+    out.writelines(f"{line12}\n{line345}\n{line67}")
